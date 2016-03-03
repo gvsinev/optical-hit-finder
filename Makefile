05393f28d126226fe019d38dc9c34b276c103a94
Gleb Sinev <gleb.sinev@duke.edu>
2016-03-03 16:03:56 -0600

    Use a FHiCL file to read hit finder parameters.

--- Makefile
+++ Makefile
@@ -6,14 +6,16 @@
 OBJS     := RunHitFinder.o
 CXX      := g++
 CXXFLAGS := -std=c++14 -Wall
+INCLUDES := -I${FHICLCPP_INC} -I${CETLIB_INC} -I${CPP0X_INC} -I${BOOST_INC}
 LDFLAGS  := -Wall
+LIBS     := -L${FHICLCPP_LIB} -lfhiclcpp -L${CETLIB_LIB} -lcetlib
 RM       := rm
 
 run-hit-finder: $(OBJS)
-	$(CXX) $(LDFLAGS) -o run-hit-finder $(OBJS)
+	$(CXX) $(LDFLAGS) -o run-hit-finder $(OBJS) $(LIBS)
 
 RunHitFinder.o: RunHitFinder.cxx
-	$(CXX) -c $(CXXFLAGS) RunHitFinder.cxx
+	$(CXX) -c $(CXXFLAGS) RunHitFinder.cxx $(INCLUDES)
 
 clean:
 	$(RM) $(OBJS) run-hit-finder
--- RunHitFinder.cxx
+++ RunHitFinder.cxx
@@ -6,14 +6,50 @@
 // Gleb Sinev, 2016, gleb.v.sinev@gmail.com
 //==============================================================================
 
+// FHiCL-CPP -- input parameters for the hit finder
+#include "fhiclcpp/ParameterSet.h"
+#include "fhiclcpp/make_ParameterSet.h"
+
+// Cetlib includes
+#include "cetlib/filepath_maker.h"
+
 // C++ includes
 #include <iostream>
+#include <string>
 
+// Function declarations
+fhicl::ParameterSet GetParameterSet(std::string const& FHiCLFileName);
+
+//------------------------------------------------------------------------------
+//int main(int argc, char *argv[])
 int main()
 {
 
+//  int numberOfArguments = 2;
+//  if (argc < numberOfArguments) 
+//  {
+//    std::cout << "Usage: run-hit-finder FHiCL-file.fcl.\n";
+//    return 1;
+//  }
+//
   std::cout << "Starting hit finder.\n";
 
+  std::string FHiCLFileName("./pdreco.fcl"); 
+  fhicl::ParameterSet parameterSet = GetParameterSet(FHiCLFileName);
+
   return 0;
+
+}
+
+//------------------------------------------------------------------------------
+fhicl::ParameterSet GetParameterSet(std::string const& FHiCLFileName)
+{
+
+  // I think this is the way to read a FHiCL file
+  fhicl::ParameterSet parameterSet;
+  cet::filepath_maker filepathMaker;
+  fhicl::make_ParameterSet(FHiCLFileName, filepathMaker, parameterSet);
+
+  return parameterSet;
 
 }
