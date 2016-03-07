0acbd411d987e253b4ebb74b19887cad5968beff
Gleb Sinev <gleb.sinev@duke.edu>
2016-03-06 20:59:39 -0600

    Open file waveform.root and save one waveform from there as ROOT macro test.C.

--- Makefile
+++ Makefile
@@ -6,10 +6,17 @@
 OBJS     := RunHitFinder.o
 CXX      := g++
 CXXFLAGS := -std=c++14 -Wall
-INCLUDES := -I${FHICLCPP_INC} -I${CETLIB_INC} -I${CPP0X_INC} -I${BOOST_INC}
 LDFLAGS  := -Wall
-LIBS     := -L${FHICLCPP_LIB} -lfhiclcpp -L${CETLIB_LIB} -lcetlib
 RM       := rm
+
+
+FHICL_INCS   := -I${FHICLCPP_INC} -I${CETLIB_INC} -I${CPP0X_INC} -I${BOOST_INC}
+LARSOFT_INCS := -I${LARDATA_INC}
+INCLUDES     := $(FHICL_INCS) $(LARSOFT_INCS) -I${ROOT_INC}
+
+FHICL_LIBS   := -L${FHICLCPP_LIB} -lfhiclcpp -L${CETLIB_LIB} -lcetlib
+LARSOFT_LIBS := -L${LARDATA_LIB} -llardata_RawData
+LIBS         := $(FHICL_LIBS) $(LARSOFT_LIBS) $(shell root-config --libs)
 
 run-hit-finder: $(OBJS)
 	$(CXX) $(LDFLAGS) -o run-hit-finder $(OBJS) $(LIBS)
--- RunHitFinder.cxx
+++ RunHitFinder.cxx
@@ -10,15 +10,25 @@
 #include "fhiclcpp/ParameterSet.h"
 #include "fhiclcpp/make_ParameterSet.h"
 
-// Cetlib includes
+// CETLib includes
 #include "cetlib/filepath_maker.h"
+
+// LArSoft includes
+#include "lardata/RawData/OpDetWaveform.h"
+
+// ROOT includes
+#include "TFile.h"
+#include "TH1D.h"
 
 // C++ includes
 #include <iostream>
 #include <string>
+#include <vector>
 
 // Function declarations
 fhicl::ParameterSet GetParameterSet(std::string const& FHiCLFileName);
+TH1D GetHistogram(TFile            & ROOTFile,
+                  std::string const& histogramName);
 
 //------------------------------------------------------------------------------
 //int main(int argc, char *argv[])
@@ -37,6 +47,15 @@ int main()
   std::string FHiCLFileName("./pdreco.fcl"); 
   fhicl::ParameterSet parameterSet = GetParameterSet(FHiCLFileName);
 
+  std::string ROOTFileName("./waveforms.root"); 
+  TFile inputFile(ROOTFileName.c_str(), "READ");
+  std::string histogramName("event_1_opchannel_0_waveform_0");
+  TH1D histogram = GetHistogram(inputFile, histogramName);
+
+  std::string outputFigure("test");
+  std::string format("C");
+  histogram.SaveAs((outputFigure + '.' + format).c_str(), format.c_str());
+
   return 0;
 
 }
@@ -51,5 +70,20 @@ fhicl::ParameterSet GetParameterSet(std::string const& FHiCLFileName)
   fhicl::make_ParameterSet(FHiCLFileName, filepathMaker, parameterSet);
 
   return parameterSet;
+
+}
+
+//------------------------------------------------------------------------------
+// Input is a LArSoft file with waveform histograms
+TH1D GetHistogram(TFile            & ROOTFile,
+                  std::string const& histogramName)
+{
+
+  std::string directoryName("opdigiana");
+  TH1D *histogram = 
+    dynamic_cast< TH1D* >(ROOTFile.GetDirectory(directoryName.c_str())
+                                 ->Get(histogramName.c_str()));
+
+  return *histogram;
 
 }
